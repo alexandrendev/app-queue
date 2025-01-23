4d225f836efa8cf10d2e410f123478d70408c649
alexandrendev <alxfn86@gmail.com>
2025-01-22 23:06:33 -0300

    initial home_atendente page

---
+++ app_queue/lib/pages/home_atendente.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeAtendente extends StatelessWidget {
+  const HomeAtendente({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
