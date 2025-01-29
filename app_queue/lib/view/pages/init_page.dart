6ba50213f8f9025cae78b7f1a75c2cc34eb6a020
alexandrendev <alxfn86@gmail.com>
2025-01-29 17:00:54 -0300

    feat: add validação de cargo do usuário

--- app_queue/lib/view/pages/init_page.dart
+++ app_queue/lib/view/pages/init_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_queue/model/user/cargo.dart';
 import 'package:app_queue/view/components/my_botton_app_bar.dart';
 import 'package:app_queue/view/components/my_up_app_bar.dart';
 import 'package:app_queue/view/pages/cadastro_paciente.dart';
@@ -7,6 +8,7 @@ import 'package:app_queue/view/pages/home_consultorio.dart';
 import 'package:app_queue/view/pages/informacoes_paciente.dart';
 import 'package:app_queue/view/pages/triagem.dart';
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
 class InitalPage extends StatefulWidget {
   const InitalPage({super.key});
@@ -18,24 +20,37 @@ class InitalPage extends StatefulWidget {
 class _InitalPageState extends State<InitalPage> {
   int _selectedIndex = 0;
   final List _pages = [];
-  String role = 'enfermeiro';
+  Cargo? role;
+
+  Future<int?> getUserRole() async {
+    final user = await ParseUser.currentUser() as ParseUser?;
+    return user?.get<int>('cargo');
+  }
 
   @override
   void initState() {
     super.initState();
-    validUserRole();
+    loadUserRole();
+  }
+
+  void loadUserRole() async {
+    var roleNum = await getUserRole();
+    if (roleNum != null) {
+      setState(() {
+        role = Cargo.fromValor(roleNum);
+        validUserRole();
+      });
+    }
   }
 
   void validUserRole() {
-    if (role == 'atendente') {
+    if (role == Cargo.atendente) {
       _pages.add(FichaListScreen());
-      // _pages.add(HomeAtendente());
       _pages.add(CadastroPaciente());
-    } else if (role == 'enfermeiro') {
-      // _pages.add(HomeConsultorio());
+    } else if (role == Cargo.enfermeiro) {
       _pages.add(FichaListScreen());
       _pages.add(InformacoesPaciente());
-    } else if (role == 'medico') {
+    } else if (role == Cargo.medico) {
       _pages.add(HomeConsultorio());
       _pages.add(CadastroPaciente());
     }
@@ -49,6 +64,12 @@ class _InitalPageState extends State<InitalPage> {
 
   @override
   Widget build(BuildContext context) {
+    if (_pages.isEmpty) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return Scaffold(
       appBar: MyUpAppBar(),
       bottomNavigationBar: MyBottomBar(_selectPage, _selectedIndex),
--- app_queue/lib/view/themes/light_theme.dart
+++ app_queue/lib/view/themes/light_theme.dart
@@ -1,29 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-    surface: Color(
-        0xFFF5F5F5), // Cinza muito claro para evitar branco puro (melhor conforto visual)
-    primary: Color(0xFFD32F2F), // Vermelho forte, mas não muito saturado
-    secondary: Color(0xFFFFCDD2), // Vermelho claro para detalhes sutis
-    inversePrimary: Colors.white, // Melhor para contraste reverso
-    onPrimary: Colors.white, // Mantém alta legibilidade sobre a cor principal
-    onSecondary: Colors.black, // Garante bom contraste sobre cor secundária
+    surface: Color(0xFFF5F5F5),
+    primary: Color(0xFFD32F2F),
+    secondary: Color(0xFFFFCDD2),
+    inversePrimary: Colors.white,
+    onPrimary: Colors.white,
+    onSecondary: Colors.black,
   ),
   textTheme: ThemeData.light().textTheme.apply(
-        bodyColor: Colors.black, // Garante boa leitura
-        displayColor: Colors.grey
-            .shade800, // Cinza escuro para melhor suavidade em textos grandes
+        bodyColor: Colors.black,
+        displayColor: Colors.grey.shade800,
       ),
   iconTheme: IconThemeData(
-    color: Color(0xFFD32F2F), // Ícones em vermelho forte para destaque
+    color: Color(0xFFD32F2F),
   ),
   bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    selectedItemColor:
-        Color(0xFFD32F2F), // Vermelho primário para destaque nos itens ativos
-    unselectedItemColor: Colors
-        .grey.shade600, // Cinza médio para melhor distinção entre ativo/inativo
-    backgroundColor: Color(0xFFFFFFFF), // Mantém branco para um visual limpo
+    selectedItemColor: Color(0xFFD32F2F),
+    unselectedItemColor: Colors.grey.shade600,
+    backgroundColor: Color(0xFFFFFFFF),
   ),
 );
